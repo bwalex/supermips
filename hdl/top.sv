38ae486942daac7da1cf447096f3b6bf3436496d
Alex Hornung <ahornung@gmail.com>
2012-06-18 17:28:42 +0100

    add temporary top-level file

---
+++ hdl/top.sv
@@ -0,0 +1,115 @@
+`timescale 1ns/10ps
+
+module top#(
+           parameter IMEM_FILE = "../software/simple.vmem",
+           parameter DMEM_FILE = "../software/simple.vmem"
+)(
+);
+
+  /*AUTOWIRE*/
+  // Beginning of automatic wires (for undeclared instantiated-module outputs)
+  wire [31:0]           dcache_addr;            // From CPU of pipeline.v
+  wire                  dcache_rd;              // From CPU of pipeline.v
+  wire                  dcache_wr;              // From CPU of pipeline.v
+  wire [31:0]           dcache_wr_data;         // From CPU of pipeline.v
+  wire [31:0]           dcache_data;
+  wire                  dcache_waitrequest;
+  wire [31:0]           icache_addr;            // From CPU of pipeline.v
+  wire                  icache_rd;              // From CPU of pipeline.v
+  wire [31:0]           icache_data;
+  wire                  icache_waitrequest;
+
+  wire [4:0]            rfile_rd_addr1;         // From CPU of pipeline.v
+  wire [4:0]            rfile_rd_addr2;         // From CPU of pipeline.v
+  wire [4:0]            rfile_wr_addr1;         // From CPU of pipeline.v
+  wire [31:0]           rfile_wr_data1;         // From CPU of pipeline.v
+  wire                  rfile_wr_enable1;       // From CPU of pipeline.v
+  // End of automatics
+  wire [31:0]           rfile_rd_data1;
+  wire [31:0]           rfile_rd_data2;
+
+
+  pipeline CPU(
+               // Outputs
+               .icache_addr             (icache_addr),
+               .icache_rd               (icache_rd),
+               .rfile_rd_addr1          (rfile_rd_addr1),
+               .rfile_rd_addr2          (rfile_rd_addr2),
+               .dcache_addr             (dcache_addr),
+               .dcache_rd               (dcache_rd),
+               .dcache_wr               (dcache_wr),
+               .dcache_wr_data          (dcache_wr_data),
+               .rfile_wr_addr1          (rfile_wr_addr1),
+               .rfile_wr_enable1        (rfile_wr_enable1),
+               .rfile_wr_data1          (rfile_wr_data1),
+               // Inputs
+               .clock                   (clock),
+               .reset_n                 (reset_n),
+               .icache_data             (icache_data),
+               .icache_waitrequest      (icache_waitrequest),
+               .rfile_rd_data1          (rfile_rd_data1),
+               .rfile_rd_data2          (rfile_rd_data2),
+               .dcache_data             (dcache_data),
+               .dcache_waitrequest      (dcache_waitrequest));
+
+  rfile REGFILE(
+                // Outputs
+                .rd_data1               (rfile_rd_data1),
+                .rd_data2               (rfile_rd_data2),
+                // Inputs
+                .clock                  (clock),
+                .reset_n                (reset_n),
+                .rd_addr1               (rfile_rd_addr1),
+                .rd_addr2               (rfile_rd_addr2),
+                .wr_addr1               (rfile_wr_addr1),
+                .wr_enable1             (rfile_wr_enable1),
+                .wr_data1               (rfile_wr_data1));
+
+  tcm #(
+            .MEM_FILE(IMEM_FILE)
+          ) ITCM (
+           // Outputs
+           .cpu_rd_data                 (icache_data),
+           .cpu_waitrequest             (icache_waitrequest),
+           // Inputs
+           .clock                       (clock),
+           .reset_n                     (reset_n),
+           .cpu_addr                    (icache_addr),
+           .cpu_wr_data                 ('b0),
+           .cpu_wr_be                   ('b0),
+           .cpu_rd                      (icache_rd),
+           .cpu_wr                      (1'b0));
+
+  tcm #(
+             .MEM_FILE(DMEM_FILE)
+           ) DTCM (
+           // Outputs
+           .cpu_rd_data                 (dcache_data[DATA_WIDTH-1:0]),
+           .cpu_waitrequest             (dcache_waitrequest),
+           // Inputs
+           .clock                       (clock),
+           .reset_n                     (reset_n),
+           .cpu_addr                    (dcache_addr[ADDR_WIDTH-1:0]),
+           .cpu_wr_data                 (dcache_wr_data[DATA_WIDTH-1:0]),
+           .cpu_wr_be                   ('b0),//dcache_wr_be[BE_WIDTH-1:0]),
+           .cpu_rd                      (dcache_rd),
+           .cpu_wr                      (dcache_wr));
+
+
+
+
+  // 100 MHz clock
+  always
+  begin
+         clock = 0;
+    #5   clock = 1;
+    #5   clock = 0;
+  end
+
+  initial begin
+        reset_n = 1;
+    #5  reset_n = 0;
+    #16 reset_n = 1;
+  end
+
+endmodule
