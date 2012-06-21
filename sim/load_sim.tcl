c7b89023e7dba7ef3cfd49dea4faed610fae1c69
Alex Hornung <ahornung@gmail.com>
2012-06-21 15:08:19 +0100

    sim - start with modelsim and ncverilog bits

---
+++ sim/load_sim.tcl
@@ -0,0 +1,30 @@
+# Set hierarchy variables
+set TOP_LEVEL_NAME "top"
+set HDL_BASE "../hdl"
+set SIM_BASE "../sim"
+
+proc ensure_lib { lib } { if ![file isdirectory $lib] { vlib $lib } }
+ensure_lib ./libraries/
+ensure_lib ./libraries/work/
+vmap work ./libraries/work/
+
+# Compile the additional test files
+vlog -sv $HDL_BASE/ex.sv
+vlog -sv $HDL_BASE/idec.sv
+vlog -sv $HDL_BASE/ifetch.sv
+vlog -sv $HDL_BASE/mem.sv
+vlog -sv $HDL_BASE/pipeline.sv
+vlog -sv $HDL_BASE/pipreg_ex_mem.sv
+vlog -sv $HDL_BASE/pipreg_id_ex.sv
+vlog -sv $HDL_BASE/pipreg_if_id.sv
+vlog -sv $HDL_BASE/pipreg_mem_wb.sv
+vlog -sv $HDL_BASE/rfile.sv
+vlog -sv $HDL_BASE/tcm.sv
+vlog -sv $HDL_BASE/wb.sv
+vlog -sv $HDL_BASE/top.sv
+
+# Elaborate the top-level design
+vsim -t ps -L work $TOP_LEVEL_NAME
+
+# Load the waveform "do file" macro script
+do ./wave.do
