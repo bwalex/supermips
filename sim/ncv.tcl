3a973751543b5878e784e1cda29d84576e321f30
Alex Hornung <ahornung@gmail.com>
2012-06-23 19:37:24 +0100

    register view overhaul

--- sim/ncv.tcl
+++ sim/ncv.tcl
@@ -155,15 +155,92 @@ simvision {
     window  geometry  "Registers for SuperMIPS"  1280x700+0+500
     register  using  "Registers for SuperMIPS"
 
-    register addtype -type rectangle -x0  40  -y0 10 -x1  280 -y1 430 -outline white
-    register addtype -type rectangle -x0  280 -y0 10 -x1  520 -y1 430 -outline white
-    register addtype -type rectangle -x0  520 -y0 10 -x1  760 -y1 430 -outline white
-    register addtype -type rectangle -x0  760 -y0 10 -x1 1000 -y1 430 -outline white
-    register addtype -type rectangle -x0 1000 -y0 10 -x1 1240 -y1 430 -outline white
 
-    register addtype -type text -text IF  -x0  140 -y0 -20 -fill white -fontsize regfont-18
-    register addtype -type text -text ID  -x0  380 -y0 -20 -fill white -fontsize regfont-18
-    register addtype -type text -text EX  -x0  620 -y0 -20 -fill white -fontsize regfont-18
-    register addtype -type text -text MEM -x0  860 -y0 -20 -fill white -fontsize regfont-18
-    register addtype -type text -text WB  -x0 1100 -y0 -20 -fill white -fontsize regfont-18
+    register addtype -page Page-1 -type rectangle -x0 40.0 -y0 10.0 -x1 280.0 -y1 430.0 -fill #000000 -outline white
+    register addtype -page Page-1 -type rectangle -x0 280.0 -y0 10.0 -x1 520.0 -y1 430.0 -fill #000000 -outline white
+    register addtype -page Page-1 -type rectangle -x0 520.0 -y0 10.0 -x1 760.0 -y1 430.0 -fill #000000 -outline white
+    register addtype -page Page-1 -type rectangle -x0 760.0 -y0 10.0 -x1 1000.0 -y1 430.0 -fill #000000 -outline white
+    register addtype -page Page-1 -type rectangle -x0 1000.0 -y0 10.0 -x1 1240.0 -y1 430.0 -fill #000000 -outline white
+
+    register addtype -page Page-1 -type text -text IF -x0 140 -y0 -20 -fill white -fontsize regfont-18
+    register addtype -page Page-1 -type text -text ID -x0 380 -y0 -20 -fill white -fontsize regfont-18
+    register addtype -page Page-1 -type text -text EX -x0 620.0 -y0 -20.0 -fill white -fontsize regfont-18
+    register addtype -page Page-1 -type text -text MEM -x0 860 -y0 -20 -fill white -fontsize regfont-18
+    register addtype -page Page-1 -type text -text WB -x0 1100 -y0 -20 -fill white -fontsize regfont-18
+
+    register addtype -page Page-1 -type signalname -x0 86.0 -y0 443.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[0]} ]
+    register addtype -page Page-1 -type signalname -x0 87.0 -y0 460.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[1]} ]
+    register addtype -page Page-1 -type signalname -x0 86.0 -y0 477.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[2]} ]
+    register addtype -page Page-1 -type signalname -x0 86.0 -y0 494.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[3]} ]
+    register addtype -page Page-1 -type signalname -x0 86.0 -y0 511.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[4]} ]
+    register addtype -page Page-1 -type signalname -x0 86.0 -y0 528.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[5]} ]
+    register addtype -page Page-1 -type signalname -x0 86.0 -y0 545.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[6]} ]
+    register addtype -page Page-1 -type signalname -x0 86.0 -y0 562.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[7]} ]
+    register addtype -page Page-1 -type signalname -x0 285.0 -y0 443.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[8]} ]
+    register addtype -page Page-1 -type signalname -x0 285.0 -y0 460.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[9]} ]
+    register addtype -page Page-1 -type signalname -x0 285.0 -y0 477.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[10]} ]
+    register addtype -page Page-1 -type signalname -x0 285.0 -y0 494.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[11]} ]
+    register addtype -page Page-1 -type signalname -x0 285.0 -y0 511.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[12]} ]
+    register addtype -page Page-1 -type signalname -x0 285.0 -y0 528.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[13]} ]
+    register addtype -page Page-1 -type signalname -x0 285.0 -y0 545.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[14]} ]
+    register addtype -page Page-1 -type signalname -x0 285.0 -y0 562.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[15]} ]
+    register addtype -page Page-1 -type signalname -x0 497.0 -y0 442.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[16]} ]
+    register addtype -page Page-1 -type signalname -x0 497.0 -y0 459.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[17]} ]
+    register addtype -page Page-1 -type signalname -x0 497.0 -y0 476.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[18]} ]
+    register addtype -page Page-1 -type signalname -x0 497.0 -y0 493.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[19]} ]
+    register addtype -page Page-1 -type signalname -x0 497.0 -y0 510.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[20]} ]
+    register addtype -page Page-1 -type signalname -x0 497.0 -y0 527.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[21]} ]
+    register addtype -page Page-1 -type signalname -x0 497.0 -y0 544.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[22]} ]
+    register addtype -page Page-1 -type signalname -x0 497.0 -y0 561.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[23]} ]
+    register addtype -page Page-1 -type signalname -x0 717.0 -y0 442.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[24]} ]
+    register addtype -page Page-1 -type signalname -x0 717.0 -y0 459.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[25]} ]
+    register addtype -page Page-1 -type signalname -x0 717.0 -y0 476.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[26]} ]
+    register addtype -page Page-1 -type signalname -x0 717.0 -y0 493.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[27]} ]
+    register addtype -page Page-1 -type signalname -x0 717.0 -y0 510.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[28]} ]
+    register addtype -page Page-1 -type signalname -x0 717.0 -y0 527.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[29]} ]
+    register addtype -page Page-1 -type signalname -x0 717.0 -y0 544.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[30]} ]
+    register addtype -page Page-1 -type signalname -x0 717.0 -y0 561.0 -fontsize regfont-12 -fill white [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[31]} ]
+    register addtype -page Page-1 -type signalvalue -x0 165.0 -y0 443.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[0]} ]
+    register addtype -page Page-1 -type signalvalue -x0 166.0 -y0 460.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[1]} ]
+    register addtype -page Page-1 -type signalvalue -x0 165.0 -y0 477.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[2]} ]
+    register addtype -page Page-1 -type signalvalue -x0 165.0 -y0 494.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[3]} ]
+    register addtype -page Page-1 -type signalvalue -x0 165.0 -y0 511.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[4]} ]
+    register addtype -page Page-1 -type signalvalue -x0 165.0 -y0 528.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[5]} ]
+    register addtype -page Page-1 -type signalvalue -x0 165.0 -y0 545.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[6]} ]
+    register addtype -page Page-1 -type signalvalue -x0 165.0 -y0 562.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[7]} ]
+    register addtype -page Page-1 -type signalvalue -x0 364.0 -y0 443.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[8]} ]
+    register addtype -page Page-1 -type signalvalue -x0 364.0 -y0 460.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[9]} ]
+    register addtype -page Page-1 -type signalvalue -x0 364.0 -y0 477.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[10]} ]
+    register addtype -page Page-1 -type signalvalue -x0 364.0 -y0 494.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[11]} ]
+    register addtype -page Page-1 -type signalvalue -x0 364.0 -y0 511.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[12]} ]
+    register addtype -page Page-1 -type signalvalue -x0 364.0 -y0 528.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[13]} ]
+    register addtype -page Page-1 -type signalvalue -x0 364.0 -y0 545.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[14]} ]
+    register addtype -page Page-1 -type signalvalue -x0 364.0 -y0 562.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[15]} ]
+    register addtype -page Page-1 -type signalvalue -x0 576.0 -y0 442.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[16]} ]
+    register addtype -page Page-1 -type signalvalue -x0 576.0 -y0 459.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[17]} ]
+    register addtype -page Page-1 -type signalvalue -x0 576.0 -y0 476.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[18]} ]
+    register addtype -page Page-1 -type signalvalue -x0 576.0 -y0 493.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[19]} ]
+    register addtype -page Page-1 -type signalvalue -x0 576.0 -y0 510.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[20]} ]
+    register addtype -page Page-1 -type signalvalue -x0 576.0 -y0 527.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[21]} ]
+    register addtype -page Page-1 -type signalvalue -x0 576.0 -y0 544.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[22]} ]
+    register addtype -page Page-1 -type signalvalue -x0 576.0 -y0 561.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[23]} ]
+    register addtype -page Page-1 -type signalvalue -x0 796.0 -y0 442.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[24]} ]
+    register addtype -page Page-1 -type signalvalue -x0 796.0 -y0 459.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[25]} ]
+    register addtype -page Page-1 -type signalvalue -x0 796.0 -y0 476.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[26]} ]
+    register addtype -page Page-1 -type signalvalue -x0 796.0 -y0 493.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[27]} ]
+    register addtype -page Page-1 -type signalvalue -x0 796.0 -y0 510.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[28]} ]
+    register addtype -page Page-1 -type signalvalue -x0 796.0 -y0 527.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[29]} ]
+    register addtype -page Page-1 -type signalvalue -x0 796.0 -y0 544.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[30]} ]
+    register addtype -page Page-1 -type signalvalue -x0 796.0 -y0 561.0 -fontsize regfont-12 -fill white -radix {} [subst -nobackslashes -nocommands {simulator::top.REGFILE.regfile[31]} ]
+
+    register addtype -page Page-1 -type text -x0 77.0 -y0 29.0 -fontsize regfont-12 -fill #ffff00 -text { PC }
+    register addtype -page Page-1 -type signalvalue -x0 159.0 -y0 29.0 -fontsize regfont-12 -fill #ffff00 -radix %x [subst -nobackslashes -nocommands {simulator::top.CPU.if_pc[31:0]} ]
+    register addtype -page Page-1 -type text -x0 42.0 -y0 47.0 -fontsize regfont-12 -fill #ffff00 -text { IW }
+    register addtype -page Page-1 -type signalvalue -x0 163.0 -y0 47.0 -fontsize regfont-12 -fill #ffff00 -mmap {MIPS opcodes} [subst -nobackslashes -nocommands {simulator::top.CPU.if_inst_word[31:0]} ]
+
+    register addtype -page Page-1 -type text -x0 302.0 -y0 25.0 -fontsize regfont-12 -fill #ffff00 { PC }
+    register addtype -page Page-1 -type signalvalue -x0 395.0 -y0 25.0 -fontsize regfont-12 -fill #ffff00 -radix {} [subst -nobackslashes -nocommands {simulator::top.CPU.if_pc_r[31:0]} ]
+    register addtype -page Page-1 -type text -x0 286.0 -y0 48.0 -fontsize regfont-12 -fill #ffff00 { IW }
+    register addtype -page Page-1 -type signalvalue -x0 418.0 -y0 48.0 -fontsize regfont-12 -fill #ffff00 -mmap {MIPS opcodes} [subst -nobackslashes -nocommands {simulator::top.CPU.if_inst_word_r[31:0]} ]
+
 }
