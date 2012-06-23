3ded4174f0a2b6484242beecdf4e046ee774c570
Alex Hornung <ahornung@gmail.com>
2012-06-23 14:09:07 +0100

    ncv.tcl - deduplicate using some custom Tcl

--- sim/ncv.tcl
+++ sim/ncv.tcl
@@ -5,10 +5,9 @@ simvision {
     window  geometry  "Waves for SuperMIPS"  1010x410+0+25
     waveform  using  "Waves for SuperMIPS"
 
-  # Add basic signals to wave window
-
     waveform  add  -signals  top.clock
     waveform  add  -signals  top.reset_n
+
 
     catch {group new -name IF -overlay 0}
     catch {group new -name ID -overlay 0}
@@ -17,93 +16,61 @@ simvision {
     catch {group new -name WB -overlay 0}
     catch {group new -name RFILE -overlay 0}
 
-    group using IF
-    group insert \
-        top.CPU.IF.pc \
-        top.CPU.stall_if
 
+    group using IF
     set groupId0 [waveform add -groups IF]
-    set gpGlist0 [waveform hierarchy contents $groupId0]
-    set gpID0 [lindex $gpGlist0 0]
     foreach {name attrs} [subst -nobackslashes -nocommands {
-        {top.CPU.IF.pc[31:0]} {-radix %x}
+        top.CPU.IF.pc {-radix %x}
+        top.CPU.IF.inst_word {-radix %d}
         top.CPU.stall_if {}
     }] {
+        group insert $name
         if {$attrs != ""} {
+            set gpGlist0 [waveform hierarchy contents $groupId0]
+            set gpID0 [lindex $gpGlist0 end]
             eval waveform format $gpID0 $attrs
         }
-        set gpGlist0 [lrange $gpGlist0 1 end]
-        set gpID0 [lindex $gpGlist0 0]
     }
     waveform hierarchy expand $groupId0
 
 
 
     group using ID
-    group insert \
-        top.CPU.if_pc_r \
-        top.CPU.if_inst_word_r \
-        top.CPU.id_A_fwd_from \
-        top.CPU.id_B_fwd_from \
-        top.CPU.id_A \
-        top.CPU.id_B \
-        top.CPU.id_B_need_late \
-        top.CPU.id_A_reg_valid \
-        top.CPU.id_A_reg \
-        top.CPU.id_B_reg \
-        top.CPU.id_B_reg_valid \
-        top.CPU.id_alu_inst \
-        top.CPU.id_alu_op \
-        top.CPU.id_alu_res_sel \
-        top.CPU.id_alu_set_u \
-        top.CPU.id_dest_reg \
-        top.CPU.id_dest_reg_valid \
-        top.CPU.id_imm \
-        top.CPU.id_imm_valid \
-        top.CPU.id_jmp_inst \
-        top.CPU.id_load_inst \
-        top.CPU.id_ls_op \
-        top.CPU.id_ls_sext \
-        top.CPU.id_shamt \
-        top.CPU.id_stall \
-        top.CPU.id_store_inst
-
     set groupId0 [waveform add -groups ID]
-    set gpGlist0 [waveform hierarchy contents $groupId0]
-    set gpID0 [lindex $gpGlist0 0]
     foreach {name attrs} [subst -nobackslashes -nocommands {
-        {top.CPU.if_pc_r} {}
-        {top.CPU.if_inst_word_r} {}
+        top.CPU.if_pc_r {}
+        top.CPU.if_inst_word_r {}
         top.CPU.id_A_fwd_from {}
         top.CPU.id_B_fwd_from {}
-        {top.CPU.id_A} {-radix %x}
-        {top.CPU.id_B} {-radix %x}
+        top.CPU.id_A {-radix %x}
+        top.CPU.id_B {-radix %x}
         top.CPU.id_B_need_late {}
         top.CPU.id_A_reg_valid {}
-        {top.CPU.id_A_reg} {-radix %d}
-        {top.CPU.id_B_reg} {-radix %d}
+        top.CPU.id_A_reg {-radix %d}
+        top.CPU.id_B_reg {-radix %d}
         top.CPU.id_B_reg_valid {}
         top.CPU.id_alu_inst {}
         top.CPU.id_alu_op {}
         top.CPU.id_alu_res_sel {}
         top.CPU.id_alu_set_u {}
-        {top.CPU.id_dest_reg} {-radix %d}
+        top.CPU.id_dest_reg {-radix %d}
         top.CPU.id_dest_reg_valid {}
-        {top.CPU.id_imm} {-radix %x}
+        top.CPU.id_imm {-radix %x}
         top.CPU.id_imm_valid {}
         top.CPU.id_jmp_inst {}
         top.CPU.id_load_inst {}
         top.CPU.id_ls_op {}
         top.CPU.id_ls_sext {}
-        {top.CPU.id_shamt} {-radix %d}
+        top.CPU.id_shamt {-radix %d}
         top.CPU.id_stall {}
         top.CPU.id_store_inst {}
     }] {
+        group insert $name
         if {$attrs != ""} {
+            set gpGlist0 [waveform hierarchy contents $groupId0]
+            set gpID0 [lindex $gpGlist0 end]
             eval waveform format $gpID0 $attrs
         }
-        set gpGlist0 [lrange $gpGlist0 1 end]
-        set gpID0 [lindex $gpGlist0 0]
     }
     waveform hierarchy expand $groupId0
 
@@ -119,11 +86,11 @@ simvision {
     window  geometry  "Registers for SuperMIPS"  1280x700+0+500
     register  using  "Registers for SuperMIPS"
 
-    register addtype -type rectangle -x0  40  -y0 0 -x1  280 -y1 420 -outline white
-    register addtype -type rectangle -x0  280 -y0 0 -x1  520 -y1 420 -outline white
-    register addtype -type rectangle -x0  520 -y0 0 -x1  760 -y1 420 -outline white
-    register addtype -type rectangle -x0  760 -y0 0 -x1 1000 -y1 420 -outline white
-    register addtype -type rectangle -x0 1000 -y0 0 -x1 1240 -y1 420 -outline white
+    register addtype -type rectangle -x0  40  -y0 10 -x1  280 -y1 430 -outline white
+    register addtype -type rectangle -x0  280 -y0 10 -x1  520 -y1 430 -outline white
+    register addtype -type rectangle -x0  520 -y0 10 -x1  760 -y1 430 -outline white
+    register addtype -type rectangle -x0  760 -y0 10 -x1 1000 -y1 430 -outline white
+    register addtype -type rectangle -x0 1000 -y0 10 -x1 1240 -y1 430 -outline white
 
     register addtype -type text -text IF  -x0  140 -y0 -20 -fill white -fontsize regfont-18
     register addtype -type text -text ID  -x0  380 -y0 -20 -fill white -fontsize regfont-18
