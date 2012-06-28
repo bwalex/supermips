540d309fcb8314c755b170b7d677b074a55fabda
Alex Hornung <ahornung@gmail.com>
2012-06-28 13:45:15 +0100

    adjust simulations to compile text_idec

--- sim/load_sim.tcl
+++ sim/load_sim.tcl
@@ -22,6 +22,7 @@ vlog -sv $HDL_BASE/pipreg_mem_wb.sv
 vlog -sv $HDL_BASE/rfile.sv
 vlog -sv $HDL_BASE/tcm.sv
 vlog -sv $HDL_BASE/wb.sv
+vlog -sv $HDL_BASE/text_idec.sv
 vlog -sv $HDL_BASE/top.sv
 
 # Elaborate the top-level design
