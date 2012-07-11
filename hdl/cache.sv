f2c27f16cf13ed3d8f6464a7cf16960976d6dac4
Alex Hornung <ahornung@gmail.com>
2012-07-11 16:11:01 +0100

    rewrite of cache (step 1/many)

--- hdl/cache.sv
+++ hdl/cache.sv
@@ -4,7 +4,11 @@ module generic_cache #(
             DATA_WIDTH  = 32,
             NLINES      = 256,
             NWORDS      = CLINE_WIDTH/DATA_WIDTH,
-            NWORDSLOG2  = $clog2(NWORDS)
+            NWORDSLOG2  = $clog2(NWORDS),
+            LFSR_SEED   = 11'd101,
+            ASSOC       = 4,
+            CLINE_WIDTH_BYTES = CLINE_WIDTH/8,
+            BE_WIDTH = CLINE_WIDTH_BYTES
 )(
   input                    clock,
   input                    reset_n,
@@ -12,6 +16,7 @@ module generic_cache #(
   input [ADDR_WIDTH-1:0]   cpu_addr,
   input                    cpu_rd,
   input                    cpu_wr,
+  input [BE_WIDTH-1:0]     cpu_wr_be,
   output                   cpu_rd_valid,
   output [CLINE_WIDTH-1:0] cpu_rd_line,
   output                   cpu_waitrequest,
@@ -31,21 +36,85 @@ module generic_cache #(
 
   localparam TAG_WIDTH  = ADDR_WIDTH - LINE_ADDR_WIDTH - LINE_WIDTH;
 
-  localparam FULL_CLINE_WIDTH = CLINE_WIDTH + TAG_WIDTH + 1;
+  localparam FULL_CLINE_WIDTH = CLINE_WIDTH + TAG_WIDTH + 1/*VALID*/ + 1/*DIRTY*/;
 
-  // XXX: Implement set-associativity
-  reg [FULL_CLINE_WIDTH-1:0]  clines [NLINES];
+  reg [10:0]                  lfsr;
+  wire [1:0]                  rand_bits;
+
+  wire [CLINE_WIDTH-1:0]      be_expanded;
+
+  reg [FULL_CLINE_WIDTH-1:0]  banks[NLINES][ASSOC];
+  reg [$clog2(ASSOC)-1:0]     bank_sel;
+
 
   reg [NWORDSLOG2-1:0]        wr_count;
   reg [NWORDSLOG2-1:0]        rd_count;
 
-  wire [FULL_CLINE_WIDTH-1:0] cur_cline;
+  wire [FULL_CLINE_WIDTH-1:0] bank_cur_cline[ASSOC];
+
   wire [LINE_ADDR_WIDTH-1:0]  cpu_line_addr;
   wire [TAG_WIDTH-1:0]        cpu_addr_tag;
   wire                        cache_miss;
   wire                        cache_evict;
-  wire                        cache_line_tag_miss;
-  wire                        cache_line_valid;
+
+  wire                        bank_line_valid[ASSOC];
+
+  wire                        bank_line_dirty[ASSOC];
+
+  wire                        bank_hit[ASSOC];
+
+  wire                        cache_hit;
+
+  /*
+   11-bit LFSR with basic equation x^11 + x^9 + 1, modified to generate two bits of entropy per cycle using a leap-forward structure.
+
+   octave:6> A = [ 0 1 0 0 0 0 0 0 0 0 0; 0 0 1 0 0 0 0 0 0 0 0; 0 0 0 1 0 0 0 0 0 0 0; 0 0 0 0 1 0 0 0 0 0 0; 0 0 0 0 0 1 0 0 0 0 0; 0 0 0 0 0 0 1 0 0 0 0; 0 0 0 0 0 0 0 1 0 0 0; 0 0 0 0 0 0 0 0 1 0 0; 0 0 0 0 0 0 0 0 0 1 0; 0 0 0 0 0 0 0 0 0 0 1; 1 0 1 0 0 0 0 0 0 0 0]
+A =
+   0   1   0   0   0   0   0   0   0   0   0
+   0   0   1   0   0   0   0   0   0   0   0
+   0   0   0   1   0   0   0   0   0   0   0
+   0   0   0   0   1   0   0   0   0   0   0
+   0   0   0   0   0   1   0   0   0   0   0
+   0   0   0   0   0   0   1   0   0   0   0
+   0   0   0   0   0   0   0   1   0   0   0
+   0   0   0   0   0   0   0   0   1   0   0
+   0   0   0   0   0   0   0   0   0   1   0
+   0   0   0   0   0   0   0   0   0   0   1
+   1   0   1   0   0   0   0   0   0   0   0
+
+octave:13> A^2
+ans =
+   0   0   1   0   0   0   0   0   0   0   0
+   0   0   0   1   0   0   0   0   0   0   0
+   0   0   0   0   1   0   0   0   0   0   0
+   0   0   0   0   0   1   0   0   0   0   0
+   0   0   0   0   0   0   1   0   0   0   0
+   0   0   0   0   0   0   0   1   0   0   0
+   0   0   0   0   0   0   0   0   1   0   0
+   0   0   0   0   0   0   0   0   0   1   0
+   0   0   0   0   0   0   0   0   0   0   1
+   1   0   1   0   0   0   0   0   0   0   0
+   0   1   0   1   0   0   0   0   0   0   0
+*/
+  always_ff @(posedge clock, negedge reset_n)
+    if (~reset_n)
+      lfsr     <= LFSR_SEED;
+    else begin
+      lfsr[0]  <= lfsr[2];
+      lfsr[1]  <= lfsr[3];
+      lfsr[2]  <= lfsr[4];
+      lfsr[3]  <= lfsr[5];
+      lfsr[4]  <= lfsr[6];
+      lfsr[5]  <= lfsr[7];
+      lfsr[6]  <= lfsr[8];
+      lfsr[7]  <= lfsr[9];
+      lfsr[8]  <= lfsr[10];
+      lfsr[9]  <= lfsr[0]  ^ lfsr[2];
+      lfsr[10] <= lfsr[1]  ^ lfsr[3];
+    end
+
+  assign rand_bits  = { lfsr[1], lfsr[0] };
+
 
 
   always_ff @(posedge clock, negedge reset_n)
@@ -57,19 +126,55 @@ module generic_cache #(
 
   assign cpu_line_addr = cpu_addr[ADDR_WIDTH-TAG_WIDTH-1 -: LINE_ADDR_WIDTH];
   assign cpu_addr_tag  = cpu_addr[ADDR_WIDTH-1 -: TAG_WIDTH];
-  assign cur_cline  = clines[cpu_line_addr];
 
-  assign cache_line_valid    = cur_cline[FULL_CLINE_WIDTH-1];
-  assign cache_line_tag_miss = (cur_cline[FULL_CLINE_WIDTH-2 -: TAG_WIDTH] != cpu_addr_tag);
+  genvar i;
+  generate
+    for (i = 0; i < ASSOC; i++) begin: BANKS
+      assign bank_cur_cline[i]  <=  banks[i][cpu_line_addr];
+      assign bank_line_valid[i] <=  bank_cur_cline[i][FULL_CLINE_WIDTH-1];
+      assign bank_line_dirty[i] <=  bank_cur_cline[i][FULL_CLINE_WIDTH-2];
+      assign bank_hit[i]        <=  bank_line_valid[i]
+                                 & (bank_cur_cline[i][FULL_CLINE_WIDTH-3 -: TAG_WIDTH] == cpu_addr_tag);
+    end
 
-  assign cache_miss  = ~cache_line_valid | cache_line_tag_miss;
-  assign cache_evict =  cache_line_valid & cache_line_tag_miss;
+    for (i = 0; i < BE_WIDTH; i = i+1) begin : EXP_BE
+      assign be_expanded[DATA_WIDTH-1-i*8 -: 8]  = { 8{cpu_wr_be[BE_WIDTH-1-i]} };
+    end
+  endgenerate
 
-  assign cpu_waitrequest  = cache_miss;
-  // XXX: redundant?
-  assign cpu_rd_valid  = ~cache_miss;
+  assign cache_hit  = (| bank_hit);
 
-  assign cpu_rd_line  = cur_cline[FULL_CLINE_WIDTH-1-TAG_WIDTH-1:0];
+
+  assign cpu_waitrequest  = (cpu_rd | cpu_wr) & ~cache_hit;
+
+  assign cpu_rd_valid  = cpu_rd & cache_hit;
+
+
+  always_comb begin
+    cpu_rd_line  = bank_cur_cline[0];
+    bank_sel     = 'b0;
+    for (integer k = 0; k < ASSOC; k++)
+      if (bank_hit[k]) begin
+        bank_sel     = k;
+        cpu_rd_line  = bank1_cur_cline[k][FULL_CLINE_WIDTH-3 - TAG_WIDTH : 0];
+        break;
+      end
+  end
+
+
+  always_ff @(posedge clock, negedge reset_n)
+    if (~reset_n)
+      for (integer i = 0; i < ASSOC; i++)
+        for (integer j = 0; j < NLINES; j++)
+          banks[i][j][FULL_CLINE_WIDTH-1 -: 2]  = 2'b00; /* Reset valid,dirty bits */
+    else if (cpu_wr & cache_hit) begin
+      banks[bank_sel][cpu_line_addr][CLINE_WIDTH-1:0]    <=  (bank_cur_cline[bank_sel] & ~be_expanded)
+                                                           | (cpu_wr_data              &  be_expanded);
+
+      banks[bank_sel][cpu_line_addr][FULL_CLINE_WIDTH-2] <=  1'b1; /* mark as dirty */
+    end
+
+
 
   assign mem_burst_len = NWORDS;
   assign mem_addr      = cpu_addr[ADDR_WIDTH-1:LINE_WIDTH]; /* XXX? */
@@ -78,10 +183,6 @@ module generic_cache #(
   assign mem_wr        = cache_evict;
 
 
-
-  // XXX: THIS IS VERY MUCH WIP - lots of stuff to implement
-
-  // XXX: add prefetching
 
   // Either read or write
   assert property (~(cpu_wr & cpu_rd));
