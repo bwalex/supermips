452178a4ff2050ac37b1ae45f2cb9508ab00d949
Alex Hornung <ahornung@gmail.com>
2012-07-17 17:37:27 +0100

    major software bits refactoring

--- sim/load_sim.tcl
+++ sim/load_sim.tcl
@@ -2,6 +2,7 @@
 set TOP_LEVEL_NAME "top"
 set HDL_BASE "../hdl"
 set SIM_BASE "../sim"
+set VLOG_FLAGS "+define+TRACE_ENABLE"
 
 proc ensure_lib { lib } { if ![file isdirectory $lib] { vlib $lib } }
 ensure_lib ./libraries/
@@ -9,22 +10,22 @@ ensure_lib ./libraries/work/
 vmap work ./libraries/work/
 
 # Compile the additional test files
-vlog -sv $HDL_BASE/types.sv
-vlog -sv $HDL_BASE/ex.sv
-vlog -sv $HDL_BASE/idec.sv
-vlog -sv $HDL_BASE/ifetch.sv
-vlog -sv $HDL_BASE/mem.sv
-vlog -sv $HDL_BASE/pipeline.sv
-vlog -sv $HDL_BASE/pipreg_ex_mem.sv
-vlog -sv $HDL_BASE/pipreg_id_ex.sv
-vlog -sv $HDL_BASE/pipreg_if_id.sv
-vlog -sv $HDL_BASE/pipreg_mem_wb.sv
-vlog -sv $HDL_BASE/rfile.sv
-vlog -sv $HDL_BASE/tcm.sv
-vlog -sv $HDL_BASE/wb.sv
-vlog -sv $HDL_BASE/text_idec.sv
-vlog -sv $HDL_BASE/trickbox.sv
-vlog -sv $HDL_BASE/top.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/types.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/ex.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/idec.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/ifetch.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/mem.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/pipeline.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/pipreg_ex_mem.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/pipreg_id_ex.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/pipreg_if_id.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/pipreg_mem_wb.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/rfile.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/tcm.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/wb.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/text_idec.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/trickbox.sv
+vlog $VLOG_FLAGS -sv $HDL_BASE/top.sv
 
 # Elaborate the top-level design
 vsim -t ps -L work $TOP_LEVEL_NAME
