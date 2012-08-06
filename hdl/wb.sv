1f50cb43ea6fa0fa81caddb3fc931b59240d0d08
Alex Hornung <ahornung@gmail.com>
2012-08-06 06:21:58 +0100

    wb - superscalar

--- hdl/wb.sv
+++ hdl/wb.sv
@@ -3,16 +3,31 @@ module wb #(
   input         clock,
   input         reset_n,
 
-  input [31:0]  result,
-  input [ 4:0]  dest_reg,
-  input         dest_reg_valid,
+  output        consume,
+  output [ 1:0] consume_count,
+  input         rob_entry_t slot_data[4],
+  input         slot_valid[4],
+  input         empty,
 
-  output [ 4:0] rfile_wr_addr1,
-  output        rfile_wr_enable1,
-  output [31:0] rfile_wr_data1
+  output [ 4:0] rfile_wr_addr[4],
+  output        rfile_wr_enable[4],
+  output [31:0] rfile_wr_data[4]
 );
 
-  assign rfile_wr_enable1 = dest_reg_valid;
-  assign rfile_wr_addr1   = dest_reg;
-  assign rfile_wr_data1   = result;
+
+  genvar        i;
+  generate
+    for (i = 0; i < 4; i++) begin
+      assign rfile_wr_addr[i]    = slot_data[i].dest_reg;
+      assign rfile_wr_enable[i]  = slot_data[i].dest_reg_valid & slot_valid[i];
+      assign rfile_wr_data[i]    = slot_data[i].result_lo;
+    end
+  endgenerate
+
+  assign consume        = 1'b1;
+  assign consume_count  =   (slot_valid[0] & slot_valid[1] & slot_valid[2] & slot_valid[3]) ? 2'd3
+                          : (slot_valid[0] & slot_valid[1] & slot_valid[2])                 ? 2'd2
+                          : (slot_valid[0] & slot_valid[1])                                 ? 2'd1
+                          : (slot_valid[0])                                                 ? 2'd0
+                          ;
 endmodule
