62bf5d1b0d95a30fba5c71ff24033d659bb7a479
Alex Hornung <ahornung@gmail.com>
2012-08-22 20:40:18 +0100

    more careful ifdef'ing

--- hdl/iss.sv
+++ hdl/iss.sv
@@ -275,7 +275,7 @@ module iss#(
       end
 
       if   (!di_ops_ready[i]
-`ifdef ROB_FORWARDING
+`ifdef ROB_FORWARDING_ENABLE
         && !(di_ops_almost_ready[i] && !(di[i].branch_inst || di[i].jmp_inst))
 `endif
       ) begin
@@ -316,9 +316,14 @@ module iss#(
         ls_B                    = di_B[i];
         ls_rob_slot             = rob_slot[i];
 	      ls_speculative          = spec;
+`ifdef ROB_FORWARDING_ENABLE
         ls_fwd_info.A_fwd       = ~di_A_valid[i];
-        ls_fwd_info.A_rob_slot  = as_aval_idx[i];
         ls_fwd_info.B_fwd       = ~di_B_valid[i];
+`else
+        ls_fwd_info.A_fwd       = 1'b0;
+        ls_fwd_info.B_fwd       = 1'b0;
+`endif
+        ls_fwd_info.A_rob_slot  = as_aval_idx[i];
         ls_fwd_info.B_rob_slot  = as_bval_idx[i];
         consumed++;
       end
@@ -329,9 +334,14 @@ module iss#(
         exmul1_B                    = di_B[i];
         exmul1_rob_slot             = rob_slot[i];
 	      exmul1_speculative          = spec;
+`ifdef ROB_FORWARDING_ENABLE
         exmul1_fwd_info.A_fwd       = ~di_A_valid[i];
-        exmul1_fwd_info.A_rob_slot  = as_aval_idx[i];
         exmul1_fwd_info.B_fwd       = ~di_B_valid[i];
+`else
+        exmul1_fwd_info.A_fwd       = 1'b0;
+        exmul1_fwd_info.B_fwd       = 1'b0;
+`endif
+        exmul1_fwd_info.A_rob_slot  = as_aval_idx[i];
         exmul1_fwd_info.B_rob_slot  = as_bval_idx[i];
         consumed++;
       end
@@ -344,9 +354,14 @@ module iss#(
             ex_B[k]                    = di_B[i];
             ex_rob_slot[k]             = rob_slot[i];
 	          ex_speculative[k]          = spec;
+`ifdef ROB_FORWARDING_ENABLE
             ex_fwd_info[k].A_fwd       = ~di_A_valid[i];
-            ex_fwd_info[k].A_rob_slot  = as_aval_idx[i];
             ex_fwd_info[k].B_fwd       = ~di_B_valid[i];
+`else
+            ex_fwd_info[k].A_fwd  = 1'b0;
+            ex_fwd_info[k].B_fwd  = 1'b0;
+`endif
+            ex_fwd_info[k].A_rob_slot  = as_aval_idx[i];
             ex_fwd_info[k].B_rob_slot  = as_bval_idx[i];
             break;
           end
@@ -360,9 +375,14 @@ module iss#(
         exmul1_B                    = di_B[i];
         exmul1_rob_slot             = rob_slot[i];
 	      exmul1_speculative          = spec;
+`ifdef ROB_FORWARDING_ENABLE
         exmul1_fwd_info.A_fwd       = ~di_A_valid[i];
-        exmul1_fwd_info.A_rob_slot  = as_aval_idx[i];
         exmul1_fwd_info.B_fwd       = ~di_B_valid[i];
+`else
+        exmul1_fwd_info.A_fwd       = 1'b0;
+        exmul1_fwd_info.B_fwd       = 1'b0;
+`endif
+        exmul1_fwd_info.A_rob_slot  = as_aval_idx[i];
         exmul1_fwd_info.B_rob_slot  = as_bval_idx[i];
         consumed++;
       end
