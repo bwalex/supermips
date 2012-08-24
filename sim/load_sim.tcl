bcac72d844cf67f04bc8764315b8ca5084123dd4
Alex Hornung <ahornung@gmail.com>
2012-08-24 14:23:18 +0100

    bit more work on modelsim compatibility

--- sim/load_sim.tcl
+++ sim/load_sim.tcl
@@ -2,7 +2,7 @@
 set TOP_LEVEL_NAME "top"
 set HDL_BASE "../hdl"
 set SIM_BASE "../sim"
-set VLOG_FLAGS "+define+REAL_CACHE+ROB_TRACE_ENABLE+IQ_TRACE_ENABLE+clogb2=\$clog2"
+set VLOG_FLAGS "+define+REAL_CACHE+ROB_TRACE_DISABLE+IQ_TRACE_ENABLE+clogb2=\$clog2"
 set VSIM_FLAGS "-gtop/MEM_FILE=\"../software/dhry.vmem\""
 
 proc ensure_lib { lib } { if ![file isdirectory $lib] { vlib $lib } }
@@ -11,31 +11,30 @@ ensure_lib ./libraries/work/
 vmap work ./libraries/work/
 
 # Compile the additional test files
-vlog $VLOG_FLAGS -sv	$SIM_BASE/modelsim_helpers.sv \
-			$HDL_BASE/types.sv \
-			$HDL_BASE/ex.sv \
-			$HDL_BASE/exmul.sv \
-			$HDL_BASE/generic_shifter.sv \
-			$HDL_BASE/idec.sv \
-			$HDL_BASE/id.sv \
-			$HDL_BASE/ifetch.sv \
-			$HDL_BASE/agu.sv \
-			$HDL_BASE/mem.sv \
-			$HDL_BASE/cache.sv \
-			$HDL_BASE/mem_arb.sv \
-			$HDL_BASE/memory.sv \
-			$HDL_BASE/pipeline.sv \
-			$HDL_BASE/iss.sv \
-			$HDL_BASE/ls_wrapper.sv \
-			$HDL_BASE/ex_wrapper.sv \
-			$HDL_BASE/ex_mul_wrapper.sv \
-			$HDL_BASE/circ_buf.sv \
-			$HDL_BASE/rob.sv \
-			$HDL_BASE/rfile.sv \
-			$HDL_BASE/tcm.sv \
-			$HDL_BASE/wb.sv \
-			$HDL_BASE/trickbox.sv \
-			$HDL_BASE/top.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/types.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/ex.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/exmul.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/generic_shifter.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/idec.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/id.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/ifetch.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/agu.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/mem.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/cache.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/mem_arb.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/memory.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/pipeline.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/iss.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/ls_wrapper.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/ex_wrapper.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/ex_mul_wrapper.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/circ_buf.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/rob.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/rfile.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/tcm.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/wb.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/trickbox.sv
+vlog $VLOG_FLAGS -sv	$HDL_BASE/top.sv
 
 # Elaborate the top-level design
 vsim $VSIM_FLAGS -t ps -L work $TOP_LEVEL_NAME
