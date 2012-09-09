098c5f6b27b04bcf1dbc6af2931ae5919072e18f
Alex Hornung <ahornung@gmail.com>
2012-09-09 11:30:35 +0100

    udpated analysis script, coverage target

--- sim/analyze.pl
+++ sim/analyze.pl
@@ -21,6 +21,9 @@ sub mean { return @_ ? sum(@_) / @_ : 0 }
   no => 0
 );
 
+%b = ();
+$branchjmp_count = 0;
+
 open(FD, $ARGV[0]) or die "Could not open file";
 while (<FD>) {
 	if ($_ =~ m/.*OP_NOT_READY.*/) {
@@ -28,7 +31,10 @@ while (<FD>) {
 		$time = $1;
 		$brj = $2;
 		$pc = $3;
-		++$opnotready{$brj};
+		if ($b{hex($pc)} == 0) {
+			++$opnotready{$brj};
+			$b{hex($pc)} = 1;
+		}
 	} elsif ($_ =~ m/.*UNIT_NOT_READY.*/) {
 		$_ =~ /\s*(\d+).*ISS: (branch|jmp), pc=\s*([0-9a-f]+).*/;
 		$time = $1;
@@ -41,17 +47,30 @@ while (<FD>) {
 		$issue_count = $2;
 		push(@isscount, $issue_count);
 	} elsif ($_ =~ m/.*issuing.*fwd.*fwd.*/) {
-		$_ =~ /\s*(\d+).*ISS: issuing.*fwd=(\d).*fwd=(\d).*/;
+		$_ =~ /\s*(\d+).*ISS: issuing to (\w+):.*pc=\s*([0-9a-f]+).*fwd=(\d).*fwd=(\d).*/;
 		$time = $1;
-		$fwdA = $2;
-		$fwdB = $3;
+		$unit = $2;
+		$pc = $3;
+		$fwdA = $4;
+		$fwdB = $5;
 		if ($fwdA != 0 or $fwdB != 0) {
 			++$forwarding{yes};
 		} else {
 			++$forwarding{no};
 		}
+	} elsif ($_ =~ m/.*issuing.*/) {
+		$_ =~ /\s*(\d+).*ISS: issuing to (\w+):.*pc=\s*([0-9a-f]+).*/;
+		$time = $1;
+		$unit = $2;
+		$pc = $3;
+		if ($unit eq "BRANCH") {
+			++$branchjmp_count;
+			$b{hex($pc)} = 0;
+		}
 	}
 }
+
+print "issued branches + jmps: $branchjmp_count\n";
 
 print "OP_NOT_READY:\n";
 while (($key, $value) = each %opnotready) {
